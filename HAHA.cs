c5a71f0971d7ab4ebb7cc435ea9bfd666ffec493
Orange Group Tech <63195743+orangegrouptech@users.noreply.github.com>
2025-04-15 18:43:16 +0800

    Add acceleration multiplier for larger displays

--- HAHA.cs
+++ HAHA.cs
@@ -21,37 +21,39 @@ namespace YouAreAnIdiot_UnFlash
             int height = Screen.PrimaryScreen.Bounds.Height;
             int integer1 = Conversions.ToInteger(this.MoveX.Text);
             int integer2 = Conversions.ToInteger(this.MoveY.Text);
+            int multiplier = 1;
+            if (height >= 1440) multiplier = 2;
             if (Conversions.ToDouble(this.X.Text) >= (double)num)
             {
-                this.MoveX.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                this.MoveX.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 if (Conversions.ToDouble(this.MoveY.Text) < 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 else if (Conversions.ToDouble(this.MoveY.Text) > 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
             }
             if (Conversions.ToDouble(this.Y.Text) >= (double)checked(height - this.Height))
             {
-                this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 if (Conversions.ToDouble(this.MoveX.Text) < 0.0)
-                    this.MoveX.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                    this.MoveX.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 else if (Conversions.ToDouble(this.MoveX.Text) > 0.0)
-                    this.MoveX.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                    this.MoveX.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
             }
             if (Conversions.ToDouble(this.X.Text) <= 0.0)
             {
-                this.MoveX.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                this.MoveX.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
                 if (Conversions.ToDouble(this.MoveY.Text) < 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 else if (Conversions.ToDouble(this.MoveY.Text) > 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
             }
             if (Conversions.ToDouble(this.Y.Text) <= 0.0)
             {
-                this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
                 if (Conversions.ToDouble(this.MoveY.Text) < 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(-(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd()))) * multiplier));
                 else if (Conversions.ToDouble(this.MoveY.Text) > 0.0)
-                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * 1));
+                    this.MoveY.Text = Conversions.ToString(checked(1 + (int)Math.Round((double)unchecked(34f * VBMath.Rnd())) * multiplier));
             }
             this.X.Text = Conversions.ToString(Conversions.ToDouble(this.X.Text) + (double)integer1);
             this.Y.Text = Conversions.ToString(Conversions.ToDouble(this.Y.Text) + (double)integer2);
